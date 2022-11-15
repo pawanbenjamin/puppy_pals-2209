076ecd6971509539813d538377bf39b0572eb044
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-11-15 11:15:50 -0600

    puppies router

--- api/puppies.js
+++ api/puppies.js
@@ -7,8 +7,75 @@ const prisma = require("../prisma/prisma");
 router.get(
   "/",
   asyncErrorHandler(async (req, res, next) => {
-    const puppies = await prisma.puppies.findMany();
+    const puppies = await prisma.puppies.findMany({
+      include: {
+        puppies_tricks: {
+          include: {
+            tricks: true,
+          },
+        },
+      },
+    });
     res.send(puppies);
+  })
+);
+
+// GET /api/puppies/:puppyId
+router.get(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const singlePup = await prisma.puppies.findUnique({
+      where: {
+        id: +req.params.puppyId,
+      },
+      include: {
+        puppies_tricks: {
+          include: {
+            tricks: true,
+          },
+        },
+      },
+    });
+    res.send(singlePup);
+  })
+);
+
+// POST /api/puppies
+router.post(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
+    // const { name, age, email, ownerId } = req.body;
+    const createdPup = await prisma.puppies.create({
+      data: req.body,
+    });
+    res.send(createdPup);
+  })
+);
+
+// PATCH /api/puppies/:puppyId
+router.patch(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const updatedPup = await prisma.puppies.update({
+      where: {
+        id: +req.params.puppyId,
+      },
+      data: req.body,
+    });
+    res.send(updatedPup);
+  })
+);
+
+// DELETE /api/puppies
+router.delete(
+  "/:puppyId",
+  asyncErrorHandler(async (req, res, next) => {
+    const deletedPup = await prisma.puppies.delete({
+      where: {
+        id: +req.params.puppyId,
+      },
+    });
+    res.send(deletedPup);
   })
 );
 
