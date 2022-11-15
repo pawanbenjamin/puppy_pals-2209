2d8f58456b777b55be7b374dc29444fcd8021625
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-11-15 11:42:50 -0600

    fixing trough table queries

--- api/puppies_tricks.js
+++ api/puppies_tricks.js
@@ -1,3 +1,50 @@
 const router = require("express").Router();
+const prisma = require("../prisma/prisma");
+const { asyncErrorHandler } = require("./utils");
+
+// Add a trick to puppy
+// POST /api/puppies_tricks/:puppy_id/:trick_id
+router.post(
+  "/:puppy_id/:trick_id",
+  asyncErrorHandler(async (req, res, next) => {
+    const { puppy_id, trick_id } = req.params;
+    const pt = await prisma.puppies_Tricks.create({
+      data: {
+        puppy_id: +puppy_id,
+        trick_id: +trick_id,
+      },
+    });
+    res.send(pt);
+  })
+);
+
+// Remove a trick from puppy
+// DELETE /api/puppies_tricks/:puppy_id/:trick_id
+router.delete(
+  "/:puppy_id/:trick_id",
+  asyncErrorHandler(async (req, res, next) => {
+    const { puppy_id, trick_id } = req.params;
+    const pt = await prisma.puppies_Tricks.findUnique({
+      where: {
+        puppy_id: +puppy_id,
+        AND: [
+          {
+            trick_id: +trick_id,
+          },
+        ],
+      },
+    });
+
+    // const deletedPt = await prisma.puppies_Tricks.delete({
+    //   where: {
+    //     AND: {
+    //       puppy_id: +puppy_id,
+    //       trick_id: +trick_id,
+    //     },
+    //   },
+    // });
+    res.send(pt);
+  })
+);
 
 module.exports = router;
